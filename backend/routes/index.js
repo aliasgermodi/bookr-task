91622651a168499dc15c85e8f0bc7270635e21da
aliasgermodi <modialiasger@gmail.com>
2023-05-30 00:27:25 +0300

    backend apis and logics tested with APIs

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import checkAuth from '../utils/checkAuth.js';
+import authRoutes from './auth.js';
+import usersRoutes from './users.js';
+import tasksRoutes from './tasks.js';
 
 const router = express.Router();
 
 
-router.get('/test', (req, res) => {
-    res.json('test');
-});
+// router.get('/test', (req, res) => {
+//     res.json('test');
+// });
+
+router.use('/auth', authRoutes);
+router.use('/users', checkAuth, usersRoutes);
+router.use('/tasks', checkAuth, tasksRoutes);
 
 export default router;
---
+++ backend/routes/users.js
@@ -0,0 +1,9 @@
+import express from 'express';
+// import { getUserInfo, updateUser } from '../controllers/users.js';
+
+const router = express.Router();
+
+// router.get('/getAllUsers', getAllUsers);
+// router.post('/createUser', createUser);
+
+export default router;
